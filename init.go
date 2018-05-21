2cc5fdb8725c6326852426eb7a8483a31cc0ff9b
Xian Zhuo <1722741150@qq.com>
2018-05-21 21:21:12 +0800

    date time format display nano second

--- init.go
+++ init.go
@@ -22,7 +22,7 @@ func Configure() {
 	flag.Parse()
 
 	customFormatter := new(log.TextFormatter)
-	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05.999999999"
 	customFormatter.FullTimestamp = true
 	log.SetFormatter(customFormatter)
 
